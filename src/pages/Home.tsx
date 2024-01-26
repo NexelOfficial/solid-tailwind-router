d337d1f6f47f08b6cbb1371fdc712d8a197f9fac
NexelOfficial <nathandiepeveen.nl@gmail.com>
2024-01-26 23:08:51 +0100

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return (
+    <div class="flex h-screen">
+      <div class="m-auto">
+        <h1 class="text-xl font-semibold">Solid.JS boilerplate</h1>
+        <p>Thanks for using this Solid.JS + TailwindCSS boilerplate</p>
+      </div>
+    </div>
+  );
+};
